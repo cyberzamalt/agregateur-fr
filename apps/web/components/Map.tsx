d57c0eead89981c812310afb23654b8b0e269c24
cyberzamalt <abriounicolas@gmail.com>
2025-11-06 12:18:55 +0100

    Update Map.tsx

--- apps/web/components/Map.tsx
+++ apps/web/components/Map.tsx
@@ -1,7 +1,7 @@
 'use client';
 import dynamic from 'next/dynamic';
 
-// Charge la vraie carte seulement côté client (pas de SSR)
+// Charge la vraie carte côté client uniquement (pas de SSR)
 const MapInner = dynamic(() => import('./MapInner'), { ssr: false });
 
 export default function Map() {
