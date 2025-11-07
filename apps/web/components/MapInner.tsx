4e244de9bdfefe73fabf3203be0420ea715f8a9b
cyberzamalt <abriounicolas@gmail.com>
2025-11-07 17:25:44 +0100

    Update MapInner.tsx

--- apps/web/components/MapInner.tsx
+++ apps/web/components/MapInner.tsx
@@ -1,6 +1,3 @@
+// apps/web/components/MapInner.tsx
 'use client';
-import { MapContainer as RLMapContainer, LayersControl as RLLayersControl, ScaleControl as RLScaleControl } from 'react-leaflet';
-export const MapContainer: any = RLMapContainer as any;
-export const LayersControl: any = RLLayersControl as any;
-export const ScaleControl: any = RLScaleControl as any;
-export default function MapInner(props: any) { return <div {...props} />; }
+export { default } from './Map';
